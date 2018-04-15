115cd1f82d15d0e5d598cd9ea51ab80b6079c3f0
Sebastian Gonzalez <sgonzalez11@gmail.com>
2018-04-15 15:32:21 -0300

    Code Restructuration

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,48 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFirestore } from 'angularfire2/firestore';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
-import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
-
-
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit {
+export class NewTrainingComponent implements OnInit, OnDestroy {
 
-  trainings: Observable<Exercise[]>;
+  exercises: Exercise[];
+  exerciseSubscription: Subscription;
 
-  constructor(
-    private trainingService: TrainingService,
-    private db: AngularFirestore
-  ) { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
-    this.trainings = this.db
-      .collection('availableExercises')
-      .snapshotChanges()
-      .map(docArray => {
-        // the map belows is just a javascript map function is not part of rxjs operators.
-        return docArray.map(doc => {
-          return {
-            id: doc.payload.doc.id,
-            name: doc.payload.doc.data().name,
-            duration: doc.payload.doc.data().duration,
-            calories: doc.payload.doc.data().calories,
-          };
-        });
-      });
-      // this.trainings = this.trainingService.getAvailableExercises();
+    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(exercises => this.exercises = exercises);
+    this.trainingService.fetchAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
   }
 
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
+  }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,20 +1,41 @@
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { AngularFirestore } from 'angularfire2/firestore';
+
 import { Exercise } from './exercise.model';
 
+@Injectable()
 export class TrainingService {
 
   exerciseChanged = new Subject<Exercise>();
-  private availableExercises: Exercise [] = [
-    { id: 'crunches', name: 'Crunches', duration: 10, calories: 80 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 10 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 15 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 30 },
-  ];
-  private runningExercise: Exercise;
-  exercises: Exercise[] = [];
+  exercisesChanged = new Subject<Exercise[]>();
 
-  getAvailableExercises() {
-    return this.availableExercises.slice();
+  private availableExercises: Exercise [] = [];
+  private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
+
+  constructor(private db: AngularFirestore) {}
+
+  fetchAvailableExercises() {
+    this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .map(docArray => {
+        // the map below is just a javascript map function is not part of rxjs operators.
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data().name,
+            duration: doc.payload.doc.data().duration,
+            calories: doc.payload.doc.data().calories,
+          };
+        });
+      })
+      .subscribe((exercises: Exercise[]) => {
+        console.log(exercises);
+        this.availableExercises = exercises;
+        this.exercisesChanged.next([...this.availableExercises ]);
+      });
   }
 
   startExercise(selectedId: string) {
